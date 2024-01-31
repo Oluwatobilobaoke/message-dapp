675c8f8f1a82bb7a9677aa25508b38fe89d9d8c8
pheranmie <okekunletobi@gmail.com>
2024-01-31 17:16:38 +0100

    deployed the contract

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,27 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
+
+// npx hardhat run scripts/deploy.js --network <<netowkr>>
+// npx hardhat run scripts/deploy.js --network sepolia
+//  npx hardhat verify --network sepolia 0x33304C05C5e68429E64BBA8fA6F65fCd8A218E29 "Hello, Welcome to message-dapp"
--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -7,22 +7,20 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const initialMessage = "Hello, Welcome to message-dapp";
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  const messageContract = await hre.ethers.deployContract(
+    "MyMessage",
+    [initialMessage],
+    {
+      initialMessage,
+    }
   );
+
+  await messageContract.waitForDeployment();
+
+  console.log(`Message Dapps has been deployed to ${messageContract.target}`);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
