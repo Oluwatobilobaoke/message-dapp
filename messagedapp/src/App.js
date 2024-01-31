2c787272d57f157f3e8fb8c927a461ac44fd1fcf
pheranmie <okekunletobi@gmail.com>
2024-01-31 17:28:13 +0100

    integrated read message on FE

--- messagedapp/src/App.js
+++ messagedapp/src/App.js
@@ -1,7 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contractABI from "./abi.json";
+const ethers = require("ethers");
+const contractAddress = "0x33304C05C5e68429E64BBA8fA6F65fCd8A218E29";
 
 function App() {
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+
+  async function readMessage() {
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      try {
+        const message = await contract.getMessage();
+        console.log(message);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +45,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <div>
+          <button onClick={readMessage}>Read Message</button>
+        </div>
       </header>
     </div>
   );
